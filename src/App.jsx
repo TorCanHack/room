aac232dbf1ed8b3a15bee6c648645e349ac2359d
torcanhack <tor409617@gmail.com>
2024-11-25 11:08:31 +0100

    mobile ready

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,14 @@ import logo from './images/logo.svg'
 import hamburger from './images/icon-hamburger.svg'
 import closeMenu from './images/icon-close.svg'
 import { useState } from 'react';
+import arrow from './images/icon-arrow.svg'
+import image_about_light from './images/image-about-light.jpg'
 
 const CustomNextArrow = (props) => {
   const { onClick } = props
 
   return (
-    <button className='absolute right-2 -bottom-6 transform -translate-y-1/2 z-10 bg-black w-14 h-14'
+    <button className='absolute -right-0.5 bottom-322 transform -translate-y-1/2 z-10 bg-black w-14 h-14'
     onClick={onClick}
     >
       <img src={nextArrow} alt='next arrow' className='mx-auto'/>
@@ -30,7 +32,7 @@ const CustomPrevArrow = (props) => {
   const { onClick } = props
 
   return (
-    <button className='absolute left-72 -bottom-6 transform -translate-y-1/2 z-10 bg-black w-14 h-14'
+    <button className='absolute left-17 bottom-322 transform -translate-y-1/2 z-10 bg-black w-14 h-14'
     onClick={onClick}
     >
       <img src={prevArrow} alt='next arrow' className='mx-auto'/>
@@ -64,12 +66,13 @@ function App() {
   
 
   return (
-    <div className='flex flex-col justify-center items-center bg-blue-300'>
-      <header className='flex flex-row justify-between  w-96 relative top-10 z-10'>
+    <div className='flex flex-col justify-center items-center font-spartan'>
+      <div className={`${showMenu ? 'flex' : 'hidden'} absolute top-11 bg-black h-1538 w-375 opacity-50 z-10`}></div>
+      <header className='flex flex-row  w-375 relative top-14 z-10 px-4'>
         <button onClick={handleShowMenu}>
           <img src={hamburger} alt='menu'/>
         </button>
-        <nav className={`${showMenu ? 'flex' : 'hidden'} items-center justify-between bg-white h-28 absolute z-20 w-96`}>
+        <nav className={`${showMenu ? 'flex' : 'hidden'} items-center justify-between bg-white h-28 absolute -top-11 -right-0 z-30 w-375 px-4`}>
           <button onClick={handleCloseMenu}>
             <img src={closeMenu} alt='close menu'/>
           </button>
@@ -81,29 +84,90 @@ function App() {
           </ul>
         </nav>
         
-        <img src={logo} alt='logo'/>
+        <img src={logo} alt='logo' className='mx-auto'/>
 
       </header>
-      <main className=' w-96'>
+      <main className=' w-375'>
         <Slider {...settings}>
-          <div>
-            <img src={image1} alt='hero1'/>
-          </div>
-          <div>
-            <img src={image2} alt='hero1'/>
-          </div>
-          <div>
-            <img src={image3} alt='hero1'/>
-          </div>
-        </Slider>
-        <h1>Discover innovative ways to decorate</h1> 
-        <p>We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.</p> 
+          <article>
+            <div>
+              <img src={image1} alt='hero1'/>
+            </div>
 
-        <img src={image_about_dark} alt='chair and stool'/>
+            <div className='py-12 px-4' >
+              <h1 className='text-40 font-semibold tracking-tight text-left leading-none mb-4'>Discover innovative ways to decorate</h1> 
+
+              <p className='text-left leading-5 text-mgray'>We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.</p> 
+
+              <div className='w-232 flex flex-row justify-between my-8'>
+                <p className='leading-4 tracking-wide12 font-medium'>SHOP NOW</p>
+                <button> 
+                  <img src={arrow} alt='arrow sign'/>
+                </button>
+
+              </div>
+
+              
+
+            </div>
+
+          </article>
+          <article>
+            <div>
+              <img src={image2} alt='hero1'/>
+            </div>
+
+            <div className='py-12 px-4'>
+              <h1 className='text-40 font-semibold tracking-tight text-left leading-none mb-4'>We are available all across the globe</h1>
       
-        <button>Shop now </button> We are available all across the globe With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today. Shop now Manufactured with the best materials Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office. Shop now About our furniture Our multifunctional collection blends design and function to suit your individual taste. Make each room unique, or pick a cohesive theme that best express your interests and what inspires you. Find the furniture pieces you need, from traditional to contemporary styles or anything in between. Product specialists are available to help you create your dream space. 
+              <p className='text-left leading-5 text-mgray'>With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.</p> 
       
-      
+              <div className='w-232 flex flex-row justify-between my-8'>
+                <p className='leading-4 tracking-wide12 font-medium'>SHOP NOW</p>
+                <button> 
+                  <img src={arrow} alt='arrow sign'/>
+                </button>
+
+              </div>
+
+            </div>
+
+          </article>
+
+          <article>
+            <div>
+              <img src={image3} alt='hero1'/>
+            </div>
+
+            <div className='py-12 px-4'>
+              <h1 className='text-40 font-semibold tracking-tight text-left leading-none mb-4'>Manufactured with the best materials</h1> 
+              <p className='text-left leading-5 text-mgray'>Our modern furniture store provide a high level of quality. Our company has invested in  advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.</p> 
+
+              <div className='w-232 flex flex-row justify-between my-8'>
+                <p className='leading-4 tracking-wide12 font-medium'>SHOP NOW</p>
+                <button> 
+                  <img src={arrow} alt='arrow sign'/>
+                </button>
+
+              </div>
+          </div>
+
+          </article>
+          
+          
+          
+        </Slider> 
+
+        <img src={image_about_dark} alt='chair and stool' className='-mt-10'/>
+
+        <article className='text-left py-8 px-4'>
+          <h2 className='font-bold  leading-cutsome22 tracking-wide5 text-sm mb-4'>ABOUT OUR FURNITURE</h2>
+
+          <p className='font-medium text-mgray leading-cutsome22'>Our multifunctional collection blends design and function to suit your individual taste. Make each room unique, or pick a cohesive theme that best express your interests and what inspires you. Find the furniture pieces you need, from traditional to contemporary styles or anything in between. Product specialists are available to help you create your dream space.</p>
+
+        </article>      
+
+        <img src={image_about_light} alt='white chair'/>
 
       </main>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,35 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mgray: '#A0A0A0'
+      },
+      fontFamily: {
+        spartan: ['League Spartan', 'sans-serif'],
+      },
+      fontSize: {
+        40: '2.5rem'
+      },
+      height: {
+        1538: '96rem'
+      },
+      inset: {
+        17: '17rem',
+        322: '24rem'
+      },
+      letterSpacing: {
+        wide5: '5.83px',
+        wide12: '12.5px'
+      },
+      lineHeight: {
+        cutsome22: '22px'
+      },
+      width: {
+        232: '14.5rem',
+        375: '23.438rem'
+      }
+    },
   },
   plugins: [],
 }
